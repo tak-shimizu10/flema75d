f2e496758f899f0af6d6a3642284da89e0ecee58
T-Yuto <u10.tk_tf.onnt@icloud.com>
2020-05-22 12:58:49 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  
-  root 'items#index'
-  
+  root "items#index"
+
   # 購入確認ページに飛ぶ
-  namespace :items do 
+  namespace :items do
     resources :buys, only: [:index]
   end
-
+  resources :items, only: [:index, :new]
 end
