dbb841895e3c9b565feb6342c83571af533605d6
T-Yuto <u10.tk_tf.onnt@icloud.com>
2020-05-24 15:21:02 +0900

    selectタブenumで実装(フロントのみ)
    商品詳細のtext_areaの最小サイズを設定
    ItemModelに日本語変換用のメソッドを記載
    登録する画像のサイズを指定
    htmlでselectタブ表示(f.select形式)
    application.rbにja.ymlを参照するように記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,23 @@ class Item < ApplicationRecord
   enum status: { brand_new: 1, like_new: 2, invisible_dirt: 3, a_little_dirt: 4, dirt_condition: 5, bad_condition: 6 }
   enum pay_side: { seller: 1, buyer: 2 }
   enum post_date: { shortest: 1, normal: 2, longest: 3 }
+  class << self
+    def localed_statuses
+      statuses.keys.map do |k|
+        [I18n.t("enums.Item.status.#{k}"), k]
+      end
+    end
+
+    def localed_pay_sides
+      pay_sides.keys.map do |k|
+        [I18n.t("enums.Item.pay_side.#{k}"), k]
+      end
+    end
+
+    def localed_post_dates
+      post_dates.keys.map do |k|
+        [I18n.t("enums.Item.post_date.#{k}"), k]
+      end
+    end
+  end
 end
