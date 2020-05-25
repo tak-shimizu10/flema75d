d0417d7a3a34342feea76bcdf5b40748ca53764f
T-Yuto <u10.tk_tf.onnt@icloud.com>
2020-05-26 02:20:52 +0900

    写真の複数投稿プレビュー（仮）、View (仮）

--- app/assets/javascripts/items_new.js
+++ app/assets/javascripts/items_new.js
@@ -1,18 +1,23 @@
 $(function () {
     //プレビュー生成の関数
     const buildPhotoPreview = (index, url) => {
-        const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+        const html =
+            `<div data-index= "${index}" id= "input_photo_preview">
+                <img data-index= "${index}" src= "${url}" width= "100px" height= "100px">
+                <span class= "input_photo_remove">削除</span>
+            </div>`;
         return html;
     }
     //file_field生成の関数
     const buildFileField = (index) => {
-        const html = `
-            <div data-index= "${index}" class= "input_photo_file_group">
+        const html =
+            `<label id= "input_photo_field" data-index= "${index}">
                 <input class= "input_photo_file" type= "file"
-                name="item[images_attributes][${index}][image]"
+                name= "item[images_attributes][${index}][image]"
                 id= "item_images_attributes_${index}_image photo_file_${index}"><br>
-                <div class= "input_photo_remove">削除</div>
-            </div>`;
+                <img class= "fas fa-camera" src= "/assets/icon/icon_camera-24c5a3dec3f777b383180b053077a49d0416a4137a1c541d7dd3f5ce93194dee.png">
+                <p class= "photos_input_text">ドラッグ＆ドロップ<br>またはクリックしてファイルをアップロード</p>
+            </label>`;
         return html;
     }
 
@@ -24,7 +29,7 @@ $(function () {
     $(".hidden_destroy").hide();
 
     //ファイル選択時新しいpreview, file_fieldを生成
-    $("#photos_input").on("change", ".input_photo_file", function (e) {
+    $("#input_photos_field").on("change", ".input_photo_file", function (e) {
 
         const targetIndex = $(this).parent().data("index");
         const file = e.target.files[0];
@@ -33,19 +38,22 @@ $(function () {
         if (img = $(`img[data-index= "${targetIndex}"]`)[0]) {
             img.setAttribute("src", blobUrl);
         } else {
-            $(".input_photo_previews").append(buildPhotoPreview(targetIndex, blobUrl));
-            $("#photos_input").append(buildFileField(fileIndex[0]));
+            $(`label[data-index= "${targetIndex}"]`).hide();
+            $("#input_photos_field").append(buildPhotoPreview(targetIndex, blobUrl));
+            $("#input_photos_field").append(buildFileField(fileIndex[0]));
             fileIndex.shift();
             fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
         }
     });
-    //削除ボタン
-    $("#photos_input").on("click", ".input_photo_remove", function () {
+    //削除ボタンクリックで選択された画像の削除
+    $("#input_photos_field").on("click", ".input_photo_remove", function () {
+
         const targetIndex = $(this).parent().data("index");
         const hiddenCheckbox = $(`input[data-index="${targetIndex}"].hidden_destroy`);
-
+        console.log(hiddenCheckbox);
+        console.log(this);
         $(this).parent().remove();
-        $(`img[data-index= "${targetIndex}"]`).remove();
-        if ($(".input_photo_file").length == 0) $("#photos_input").append(buildFileField(fileIndex[0]));
-    })
+
+        if ($(".input_photo_file").length == 0) $("#input_photos_field").append(buildFileField(fileIndex[0]));
+    });
 });
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :user
   belongs_to :category
