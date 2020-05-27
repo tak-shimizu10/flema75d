c43e8bdf61301c52ff29cb4034a901791b0e9ef9
T-Yuto <u10.tk_tf.onnt@icloud.com>
2020-05-27 10:58:32 +0900

    不要な部分を削除

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,12 +1,13 @@
 class ImagesController < ApplicationController
-  
   def create
     @item = Item.find(params[:item_id])
     @image = @item.images.new(image_params)
     @image.create
   end
+
   private
+
   def image_params
-    params.require(:image).permit(:image).maerge(item_id: )
+    params.require(:image).permit(:image)
   end
 end
