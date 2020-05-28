1d53be3dc4a1f6adcc4347dc55c6ec8f43cad239
T-Yuto <u10.tk_tf.onnt@icloud.com>
2020-05-28 09:27:59 +0900

    first_commit

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name           { "アイテム名" }
+    detail         { "商品詳細コメント" }
+    price          { 999 }
+    pay_side       { 1 }
+    post_date      { 1 }
+    status         { 1 }
+    prefecture_id  { 1 }
+    category_id    { 1 }
+    situation      { 0 }
+    user_id        { 1 }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#create" do
+
+    it "name, detail, price, pay_side, post_date, status, prefecture_id,  category_id, situationが存在すれば登録できること" do
+      user = create(:user)
+      category = create(:category)
+      item = build(:item, category_id: category.id, user_id: user.id)
+      expect(item).to be_valid
+    end
+
+    it
+  end
 end
