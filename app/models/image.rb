a38d8b53ee6ec3611eece39f215e0c8ea9edbe6f
watcher041 <63713236+watcher041@users.noreply.github.com>
2020-05-30 14:30:17 +0900

    edit_image_validates
    バリデーションを修正

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,5 @@ class Image < ApplicationRecord
   belongs_to :item
   mount_uploader :image, ImageUploader
 
-  validates :image, length: { minimum: 1, maximum: 10 }
+  validates :image, presence: true
 end
