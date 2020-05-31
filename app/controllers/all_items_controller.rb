a20101b378b7fcb6139b88168cb79b71c2185038
miyatata <mgino2718@gmail.com>
2020-05-31 14:34:45 +0900

    商品一覧

--- app/controllers/all_items_controller.rb
+++ app/controllers/all_items_controller.rb
@@ -3,6 +3,10 @@ class AllItemsController < ApplicationController
   def index
     @items = Item.all.order("created_at DESC")
   end
+  def category
+    @categories = Category.where(ancestry:nil)
+    @category = Category.find(params[:category_id])
+  end
 
   private
 
