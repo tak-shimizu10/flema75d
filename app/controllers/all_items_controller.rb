0696faaaaf4863b99c7453f4cc8fa9092997d9a4
watcher041 <63713236+watcher041@users.noreply.github.com>
2020-05-31 15:11:08 +0900

    edit_all_items_view
    部分テンプレートの参照先を変更したので、商品詳細一覧が表示されるように修正

--- app/controllers/all_items_controller.rb
+++ app/controllers/all_items_controller.rb
@@ -1,11 +1,10 @@
 class AllItemsController < ApplicationController
-  before_action :parent_category, only: [:index,:new,:create,:show]
+  before_action :parent_category, only: [:index,:category]
   def index
     @items = Item.all.order("created_at DESC")
   end
   
   def category
-    @categories = Category.where(ancestry:nil)
     @category = Category.find(params[:category_id])
   end
 
