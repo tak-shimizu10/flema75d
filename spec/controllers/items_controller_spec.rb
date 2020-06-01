7ab74ec7b1fdb9cc3198d9847a40d08196f52da1
miyatata <mgino2718@gmail.com>
2020-06-01 17:35:07 +0900

    トップページ単体テスト

---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe ItemsController do
+  describe 'GET #index' do
+    it "index.html.hamlに遷移すること" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
+end
