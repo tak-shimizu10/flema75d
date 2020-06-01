920bf3462fdfed60504011bd61a747e8c10b3bbd
T-Yuto <u10.tk_tf.onnt@icloud.com>
2020-06-02 01:58:49 +0900

    scroll 出品ボタン
    画面縮小時の出品ボタンを小さく
    出品編集ページで不必要にスクロールしていたのを修正

--- app/assets/javascripts/items_form.js
+++ app/assets/javascripts/items_form.js
@@ -209,9 +209,10 @@ $(function () {
     //送信ボタンクリック時、空のフォームにエラーメッセージ表示
     $(".items_form_list").on("click", ".item_submit", function () {
 
-        if ($(".input_photo_preview").length == 0)
-            showCautionMessage($(".items_form_photos"))
-        $(window).scrollTop($("#photos_input").offset().top);
+        if ($("#photos_input .input_photo_preview").length == 0) {
+            showCautionMessage($(".items_form_photos"));
+            $("html, body").scrollTop($("#photos_input").offset().top);
+        }
         if ($("#item_name").val().length == 0)
             showCautionMessage($(".items_name"));
         if ($("#item_detail").val().length == 0)
