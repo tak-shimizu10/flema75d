b9cc1e2202731ad1d813199b0fc5912b2edc20f9
T-Yuto <u10.tk_tf.onnt@icloud.com>
2020-06-03 14:53:30 +0900

    お気に入り機能実装（非同期通信　未実装）

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   def create
     like = current_user.likes.build(item_id: params[:item_id])
     like.save
-    redirect_to item_path(1)
+    redirect_to item_path(params[:item_id])
   end
 
   def destroy
     like = Like.find_by(item_id: params[:item_id], user_id: current_user.id)
     like.destroy
-    redirect_to item_path(1)
+    redirect_to item_path(params[:item_id])
   end
 end
