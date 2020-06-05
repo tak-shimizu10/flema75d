12cb97068fec8a1c5406026be1b8c6bd7336f2ce
T-Yuto <u10.tk_tf.onnt@icloud.com>
2020-06-06 03:17:36 +0900

    commit

--- app/assets/javascripts/items_form.js
+++ app/assets/javascripts/items_form.js
@@ -278,4 +278,36 @@ $(function () {
         const targetFileField = $(".input_photo_field").filter(`[data-index= "${targetIndex}"]`)[0];
         $(targetFileField).click();
     });
+    //ユーザー登録郵便番号の入力欄
+    //
+    function inputOnlyNumber(e) {
+        let string = String.fromCharCode(e.which);
+        if ("0123456789-".indexOf(string, 0) < 0) return false;
+        return true;
+    };
+    $(".field-input").on("keypress", "#address_zipcode", function (e) {
+        return inputOnlyNumber(e);
+    });
+    function insertHyphen(input) {
+        return input.slice(0, 3) + "-" + input.slice(3, input.length);
+    };
+    $(".field-input").on("keyup", "#address_zipcode", function () {
+        let input = $(this).val();
+        let key = event.keyCode || event.charCode;
+        if (key == 8 || key == 46) return false;
+        if (input.length === 3) $(this).val(insertHyphen(input));
+    });
+    $(".field-input").on("focusout", "#address_zipcode", function () {
+        let input = $(this).val();
+        if (input.length >= 3 && input.substr(3, 1 !== "-")) {
+            $(this).val(insertHyphen(input));
+        };
+    });
+    //マイページスクロール用
+    $(".account_show").on("click", ".link_page", function () {
+        window.scroll({
+            top: 200,
+            behavior: "smooth"
+        });
+    })
 });
