c9e3b65e7ba091358d19d90ef94cea38c0e3e757
watcher041 <63713236+watcher041@users.noreply.github.com>
2020-06-09 15:36:51 +0900

    edit_buyer
    前回の条件をひとつにまとめて記述

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -3,13 +3,14 @@ class BuysController < ApplicationController
   # ログインしてなければ、ログイン画面に映る
   before_action :authenticate_user!
 
-  # 商品が
-
   # 商品情報を取得
-  before_action :find_buys_info, only: [:new, :create]
+  before_action :find_buys_info
+
+  # 出品者自身あるいは購入済みの商品に対しては何もせずに戻る
+  before_action :buyer_check
 
   # payjpを使用するための認証を行う
-  before_action :payjp_sertification, only: [:new, :create]
+  before_action :payjp_sertification
 
   # payjpに関連するエラーが発生した際に例外処理を行う
   rescue_from Payjp::PayjpError, with: :payjp_error
@@ -21,39 +22,31 @@ class BuysController < ApplicationController
 
   # 商品の購入確認
   def new
-    if @item.situation == "exhibition"  && @item.user.id != current_user.id
-      @address = current_user.address
-      unless @user_cards.blank?
-        @customer_id = @user_cards.first[:customer_id]
-        customer = Payjp::Customer.retrieve(@customer_id)
-        @card_data = customer.cards.retrieve(customer.default_card)
-      end
-    else
-      redirect_to root_path
+    @address = current_user.address
+    unless @user_cards.blank?
+      @customer_id = @user_cards.first[:customer_id]
+      customer = Payjp::Customer.retrieve(@customer_id)
+      @card_data = customer.cards.retrieve(customer.default_card)
     end
   end
 
   # 商品の購入
   def create
-    if @item.situation == "exhibition" && @item.user.id != current_user.id
-      @customer_id = @user_cards.first[:customer_id]
+    @customer_id = @user_cards.first[:customer_id]
 
-      Payjp::Charge.create(
-        amount: @item[:price],
-        customer: @customer_id,
-        currency: "jpy",
-      )
+    Payjp::Charge.create(
+      amount: @item[:price],
+      customer: @customer_id,
+      currency: "jpy",
+    )
 
-      # 出品中（0）を取引中（1）に変更
-      @item.situation_transaction!
-      @item.buyer_id = current_user.id
-      if @item.save
-        redirect_to root_path
-      else
-        redirect_to new_item_buy_path(@item)
-      end
-    else
+    # 出品中（0）を取引中（1）に変更
+    @item.situation_transaction!
+    @item.buyer_id = current_user.id
+    if @item.save
       redirect_to root_path
+    else
+      redirect_to new_item_buy_path(@item)
     end
   end
 
@@ -67,6 +60,12 @@ class BuysController < ApplicationController
   def payjp_sertification
     require "payjp"
     Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+  end
+
+  def buyer_check
+    if @item.situation != "exhibition" || @item.user.id == current_user.id
+      redirect_to root_path
+    end
   end
 
   def payjp_error(event)
